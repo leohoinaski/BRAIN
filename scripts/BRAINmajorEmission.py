c6ff3b057c6b151be6f7e55baec07ae11feb04cc
Leonardo Hoinaski <leonardo.hoinaski@ufsc.br>
2024-02-06 18:47:34 -0300

    Update BRAINmajorEmission.py

--- scripts/BRAINmajorEmission.py
+++ scripts/BRAINmajorEmission.py
@@ -72,7 +72,7 @@ PM25 = {
 
 pollutants=[NO2,SO2,O3,PM10,PM25]
 emisTypes = ['BRAVES','FINN','IND2CMAQ','MEGAN']
-
+pollutants=[CO]
 #------------------------------PROCESSING--------------------------------------
 BASE = os.getcwd()
 rootFolder = os.path.dirname(os.path.dirname(BASE))
@@ -81,7 +81,7 @@ airQualityFolder =  dataFolder+'/BRAIN'
 emissFolder =  dataFolder+'/EMIS'
 domain = 'BR'
 year = '2020'
-
+majorEmitters=[]
 print('Looping for each variable')
 for kk,pol in enumerate(pollutants):
     # ======== EMIS files============
@@ -107,9 +107,11 @@ for kk,pol in enumerate(pollutants):
     for ii, emisType in enumerate(emisTypes):
         fileType='BRAIN_BASEMIS_'+domain+'_2019_'+emisType+'_'+polEmis+'_'+str(year)
         prefixed = sorted([filename for filename in os.listdir(emissFolder) if filename.startswith(fileType)])
-        ds = nc.Dataset(prefixed[0])
-        emisAve.append(np.nanpercentile(ds[polEmis][0:8759,:,:,:],50,axis=0))
-        emisMax.append(np.nanpercentile(ds[polEmis][0:8759,:,:,:],99,axis=0))
+        if len(prefixed)>0:
+            ds = nc.Dataset(prefixed[0])
+            emisAve.append(np.nanpercentile(ds[polEmis][0:8759,:,:,:],50,axis=0))
+            emisMax.append(np.nanpercentile(ds[polEmis][0:8759,:,:,:],99,axis=0))
+            
 
     dataEMIS = ds[polEmis][0:8759,:,:,:]
     latBRAIN = ds['LAT'][:]
@@ -150,4 +152,55 @@ for kk,pol in enumerate(pollutants):
         emi[:,cityMat==0] = np.nan
         emi[:,cityMat==0] = np.nan
         
-        
+    
+    majorEmitter = np.argmax(np.vstack((emisMax)),axis=0).astype(float)
+    majorEmitter[cityMat==0] = np.nan
+    majorEmitters.append(majorEmitter)
+    
+    
+    #%%
+    fig,ax = plt.subplots()
+    cm = 1/2.54  # centimeters in inches
+    fig.set_size_inches(18*cm, 18*cm)
+    cmap = matplotlib.colors.LinearSegmentedColormap.from_list("", ['#0c8b96','#f51b1b','#d4d40b','#88f268'])
+    #bounds = np.array(np.array(range(0,len(emisTypes))))
+    #norm = mpl.colors.BoundaryNorm(bounds, cmap.N)
+    heatmap = ax.pcolor(lonBRAIN,latBRAIN,majorEmitter,
+                        #norm=norm,
+                        #vmin=np.nanmin([np.nanmin(dataMERRAfiltered[:,:,:]),np.nanmin(dataBRAINinMERRA[:,:,:])]),
+                        #vmax=np.nanmax([np.nanmax(dataMERRAfiltered[:,:,:]),np.nanmax(dataBRAINinMERRA[:,:,:])]),
+                        #norm=matplotlib.colors.LogNorm(
+                        #    vmin=np.nanmin([np.nanmin(dataMERRAfiltered[:,:,:]),np.nanmin(dataBRAINinMERRA[:,:,:])])*1.2,
+                        #    vmax=np.nanmax([np.nanmax(dataMERRAfiltered[:,:,:]),np.nanmax(dataBRAINinMERRA[:,:,:])])*0.8),
+                        cmap=cmap)
+    # cbar = fig.colorbar(heatmap,fraction=0.04, pad=0.02,
+    #                     #extend='both', 
+    #                     ticks=bounds,
+    #                     #spacing='uniform',
+    #                     orientation='horizontal',
+    #                     #norm=matplotlib.colors.LogNorm(vmin=np.nanmin([np.nanmin(dataMERRAfiltered[:,:,:]),np.nanmin(dataBRAINinMERRA[:,:,:])]),
+    #                     #                               vmax=np.nanmax([np.nanmax(dataMERRAfiltered[:,:,:]),np.nanmax(dataBRAINinMERRA[:,:,:])]))
+    #                     )
+    #cbar.ax.set_xticklabels(['{:.0f}'.format(x) for x in bounds],rotation=30)
+    #cbar.ax.set_xlabel(pol['tag']+' ('+pol['Unit']+')\n a) BRAIN original', rotation=0,fontsize=6)
+   
+    # cbar.ax.get_xaxis().labelpad = 2
+    # cbar.ax.tick_params(labelsize=6)
+
+
+    xb = [np.nanmin(lonBRAIN[~np.isnan(majorEmitter)[:,:]]),
+          np.nanmax(lonBRAIN[~np.isnan(majorEmitter)[:,:]])]
+    yb = [np.nanmin(latBRAIN[~np.isnan(majorEmitter)[:,:]]),
+          np.nanmax(latBRAIN[~np.isnan(majorEmitter)[:,:]])]
+    ax.set_xlim([xb[0], xb[1]])
+    ax.set_ylim([yb[0], yb[1]])
+    ax.set_xticks([])
+    ax.set_yticks([])    
+    shape_path= rootFolder+'/shapefiles/BR_regions.shp'
+    #shape_path= '/media/leohoinaski/HDD/shapefiles/SouthAmerica.shp'
+    #shape_path= '/media/leohoinaski/HDD/shapefiles/BR_Pais_2022/BR_Pais_2022.shp'
+    dataShp = gpd.read_file(shape_path)
+    dataShp.boundary.plot(ax=ax,edgecolor='black',linewidth=0.3)
+    ax.set_frame_on(False)
+    #fig.savefig(os.path.dirname(BASE)+'/figures'+'/spatialQ_'+pol['tag']+'_'+str(pol['Criteria'])+'.png', format="png",
+    #           bbox_inches='tight',dpi=300)
