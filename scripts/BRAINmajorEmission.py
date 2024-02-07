1395653ed1f23a7137868b2d3c6781d08ed52cc4
Leonardo Hoinaski <leonardo.hoinaski@ufsc.br>
2024-02-06 22:03:47 -0300

    Update BRAINmajorEmission.py

--- scripts/BRAINmajorEmission.py
+++ scripts/BRAINmajorEmission.py
@@ -23,7 +23,7 @@ import scipy
 from shapely.geometry import Point
 import pandas as pd
 import matplotlib as mpl
-
+import matplotlib.patches as mpatches
 # -------------------------------INPUTS----------------------------------------
 
 
@@ -72,7 +72,9 @@ PM25 = {
 
 pollutants=[NO2,SO2,O3,PM10,PM25]
 emisTypes = ['BRAVES','FINN','IND2CMAQ','MEGAN']
-pollutants=[CO]
+emisNames = ['Vehicular', 'Fire', 'Industrial', 'Biogenic']
+colors =['#0c8b96','#f51b1b','#fcf803','#98e3ad']
+#pollutants=[CO]
 #------------------------------PROCESSING--------------------------------------
 BASE = os.getcwd()
 rootFolder = os.path.dirname(os.path.dirname(BASE))
@@ -82,6 +84,7 @@ emissFolder =  dataFolder+'/EMIS'
 domain = 'BR'
 year = '2020'
 majorEmitters=[]
+majorEmittersAve=[]
 print('Looping for each variable')
 for kk,pol in enumerate(pollutants):
     # ======== EMIS files============
@@ -104,14 +107,17 @@ for kk,pol in enumerate(pollutants):
     # Opening netCDF files
     emisAve = []
     emisMax = []
+    emisNamesUpdated =[]
+    updatedColors =[]
     for ii, emisType in enumerate(emisTypes):
         fileType='BRAIN_BASEMIS_'+domain+'_2019_'+emisType+'_'+polEmis+'_'+str(year)
         prefixed = sorted([filename for filename in os.listdir(emissFolder) if filename.startswith(fileType)])
         if len(prefixed)>0:
             ds = nc.Dataset(prefixed[0])
-            emisAve.append(np.nanpercentile(ds[polEmis][0:8759,:,:,:],50,axis=0))
+            emisAve.append(np.nanmean(ds[polEmis][0:8759,:,:,:],axis=0))
             emisMax.append(np.nanpercentile(ds[polEmis][0:8759,:,:,:],99,axis=0))
-            
+            emisNamesUpdated.append(emisNames[ii])
+            updatedColors.append(colors[ii])
 
     dataEMIS = ds[polEmis][0:8759,:,:,:]
     latBRAIN = ds['LAT'][:]
@@ -155,52 +161,56 @@ for kk,pol in enumerate(pollutants):
     
     majorEmitter = np.argmax(np.vstack((emisMax)),axis=0).astype(float)
     majorEmitter[cityMat==0] = np.nan
+    majorEmitter[np.nansum(emisMax,axis=0)[0,:,:]<=0]=np.nan
     majorEmitters.append(majorEmitter)
     
     
+    majorEmitterAve = np.argmax(np.vstack((emisAve)),axis=0).astype(float)
+    majorEmitterAve[cityMat==0] = np.nan
+    majorEmitterAve[np.nansum(emisAve,axis=0)[0,:,:]<=0]=np.nan
+    majorEmittersAve.append(majorEmitterAve)
+    
     #%%
-    fig,ax = plt.subplots()
-    cm = 1/2.54  # centimeters in inches
-    fig.set_size_inches(18*cm, 18*cm)
-    cmap = matplotlib.colors.LinearSegmentedColormap.from_list("", ['#0c8b96','#f51b1b','#d4d40b','#88f268'])
-    #bounds = np.array(np.array(range(0,len(emisTypes))))
-    #norm = mpl.colors.BoundaryNorm(bounds, cmap.N)
-    heatmap = ax.pcolor(lonBRAIN,latBRAIN,majorEmitter,
-                        #norm=norm,
-                        #vmin=np.nanmin([np.nanmin(dataMERRAfiltered[:,:,:]),np.nanmin(dataBRAINinMERRA[:,:,:])]),
-                        #vmax=np.nanmax([np.nanmax(dataMERRAfiltered[:,:,:]),np.nanmax(dataBRAINinMERRA[:,:,:])]),
-                        #norm=matplotlib.colors.LogNorm(
-                        #    vmin=np.nanmin([np.nanmin(dataMERRAfiltered[:,:,:]),np.nanmin(dataBRAINinMERRA[:,:,:])])*1.2,
-                        #    vmax=np.nanmax([np.nanmax(dataMERRAfiltered[:,:,:]),np.nanmax(dataBRAINinMERRA[:,:,:])])*0.8),
-                        cmap=cmap)
-    # cbar = fig.colorbar(heatmap,fraction=0.04, pad=0.02,
-    #                     #extend='both', 
-    #                     ticks=bounds,
-    #                     #spacing='uniform',
-    #                     orientation='horizontal',
-    #                     #norm=matplotlib.colors.LogNorm(vmin=np.nanmin([np.nanmin(dataMERRAfiltered[:,:,:]),np.nanmin(dataBRAINinMERRA[:,:,:])]),
-    #                     #                               vmax=np.nanmax([np.nanmax(dataMERRAfiltered[:,:,:]),np.nanmax(dataBRAINinMERRA[:,:,:])]))
-    #                     )
-    #cbar.ax.set_xticklabels(['{:.0f}'.format(x) for x in bounds],rotation=30)
-    #cbar.ax.set_xlabel(pol['tag']+' ('+pol['Unit']+')\n a) BRAIN original', rotation=0,fontsize=6)
-   
-    # cbar.ax.get_xaxis().labelpad = 2
-    # cbar.ax.tick_params(labelsize=6)
-
-
-    xb = [np.nanmin(lonBRAIN[~np.isnan(majorEmitter)[:,:]]),
-          np.nanmax(lonBRAIN[~np.isnan(majorEmitter)[:,:]])]
-    yb = [np.nanmin(latBRAIN[~np.isnan(majorEmitter)[:,:]]),
-          np.nanmax(latBRAIN[~np.isnan(majorEmitter)[:,:]])]
-    ax.set_xlim([xb[0], xb[1]])
-    ax.set_ylim([yb[0], yb[1]])
-    ax.set_xticks([])
-    ax.set_yticks([])    
     shape_path= rootFolder+'/shapefiles/BR_regions.shp'
     #shape_path= '/media/leohoinaski/HDD/shapefiles/SouthAmerica.shp'
     #shape_path= '/media/leohoinaski/HDD/shapefiles/BR_Pais_2022/BR_Pais_2022.shp'
     dataShp = gpd.read_file(shape_path)
-    dataShp.boundary.plot(ax=ax,edgecolor='black',linewidth=0.3)
-    ax.set_frame_on(False)
-    #fig.savefig(os.path.dirname(BASE)+'/figures'+'/spatialQ_'+pol['tag']+'_'+str(pol['Criteria'])+'.png', format="png",
-    #           bbox_inches='tight',dpi=300)
+    xb = [np.nanmin(lonBRAIN[~np.isnan(majorEmitter)[:,:]]),
+          np.nanmax(lonBRAIN[~np.isnan(majorEmitter)[:,:]])]
+    yb = [np.nanmin(latBRAIN[~np.isnan(majorEmitter)[:,:]]),
+          np.nanmax(latBRAIN[~np.isnan(majorEmitter)[:,:]])]
+    
+    fig,ax = plt.subplots(1,2)
+    cm = 1/2.54  # centimeters in inches
+    fig.set_size_inches(18*cm, 9*cm)
+    cmap = matplotlib.colors.ListedColormap(updatedColors)
+    bound = np.array(np.array(range(0,len(emisNamesUpdated)+1)))
+    heatmap = ax[0].pcolor(lonBRAIN,latBRAIN,majorEmitterAve,
+                        cmap=cmap)
+    # Preparing borders for the legend
+    bound_prep = np.round(bound * 7, 2)
+    # Creating 8 Patch instances
+    ax[0].legend([mpatches.Patch(color=cmap(b)) for b in bound[:-1]],
+               emisNamesUpdated,loc='lower left' ,fontsize=8, markerscale=15, frameon=False)
+    ax[0].set_xlim([xb[0], xb[1]])
+    ax[0].set_ylim([yb[0], yb[1]])
+    ax[0].set_xticks([])
+    ax[0].set_yticks([])    
+    dataShp.boundary.plot(ax=ax[0],edgecolor='black',linewidth=0.3)
+    ax[0].set_frame_on(False)
+    ax[0].text(0.05, 0.35, 'a) Average', transform=ax[0].transAxes,
+            size=8)
+    
+    heatmap = ax[1].pcolor(lonBRAIN,latBRAIN,majorEmitter,
+                        cmap=cmap)
+    ax[1].set_xlim([xb[0], xb[1]])
+    ax[1].set_ylim([yb[0], yb[1]])
+    ax[1].set_xticks([])
+    ax[1].set_yticks([])    
+    dataShp.boundary.plot(ax=ax[1],edgecolor='black',linewidth=0.3)
+    ax[1].set_frame_on(False)
+    ax[1].text(0.05, 0.35, 'b) 99° percentile', transform=ax[1].transAxes,
+            size=8)
+    fig.tight_layout()
+    fig.savefig(os.path.dirname(BASE)+'/figures'+'/majorEmitters_'+pol['tag']+'.png', format="png",
+               bbox_inches='tight',dpi=300)
